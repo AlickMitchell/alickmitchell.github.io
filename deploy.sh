dd8674ab4d6f6884efae7440bbc355b7fdf57915
Alick <mitchell.alick@gmail.com>
2024-01-13 22:49:58 +0000

    fixed error with eem post naming error. It requires that the title name is the same as the directory/filename; fixed the rsync errors in the deploy.sh, it required --no- options adding

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ USER=frank
 HOST=cablekitten
 DIR=/var/www/cablekitten.co.uk   # the directory where your web site files should go
 
-hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
+hugo && rsync -avz --delete --no-owner --no-group --no-atimes --no-times --no-perms public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
 
 ssh frank@cablekitten sudo chown -R www-data:www-data /var/www/cablekitten.co.uk/
 exit 0
